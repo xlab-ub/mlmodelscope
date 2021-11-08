8496f88b0527ff2f37d125f75baf8984d208fb63
Steve Zazeski <steve@pixotech.com>
2021-11-08 14:55:30 -0600

    Updates to the sync script

--- sync-to-s3.sh
+++ sync-to-s3.sh
@@ -3,16 +3,20 @@
 echo "sync-to-s3.sh - - - - "
 
 # Check if params are set
-${AWS_ACCESS_KEY_ID?Required environment var AWS_ACCESS_KEY_ID not set}
-${AWS_SECRET_ACCESS_KEY?Required environment var AWS_SECRET_ACCESS_KEY not set}
-${AWS_DEFAULT_REGION?Required environment var AWS_DEFAULT_REGION not set}
+if [ -z ${AWS_ACCESS_KEY_ID} ]; then echo "Required environment var AWS_ACCESS_KEY_ID not set"; exit 1; fi
+if [ -z ${AWS_SECRET_ACCESS_KEY} ]; then echo "Required environment var AWS_SECRET_ACCESS_KEY not set"; exit 1; fi
+if [ -z ${AWS_DEFAULT_REGION} ]; then echo "Required environment var AWS_DEFAULT_REGION not set"; exit 1; fi
 
 # download awscli if not already
 if [ -z `stat ./aws/dist/aws` ]; then
     echo "aws cli is not available, downloading one now"
     curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-    unzip awscliv2.zip
+    unzip -qq awscliv2.zip
 fi
+
+pwd
+ls
+ls ./aws
 
 ./aws/dist/aws s3 sync /usr/share/nginx/html s3://staging.mlmodelscope.org
 
