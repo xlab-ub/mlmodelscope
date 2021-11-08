40e37a046e5e29fac1a807f96fc2753fe10dde1f
Steve Zazeski <steve@pixotech.com>
2021-11-08 16:21:20 -0600

    switch to apt to install the aws cli

--- sync-to-s3.sh
+++ sync-to-s3.sh
@@ -10,17 +10,13 @@ if [ -z ${AWS_DEFAULT_REGION} ]; then echo "Required environment var AWS_DEFAULT
 # download awscli if not already
 if [ -z `stat ./aws/dist/aws` ]; then
     echo "aws cli is not available, downloading one now"
-    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-    unzip -qq awscliv2.zip
+    apt update && apt install awscli -y
 fi
 
-pwd
-ls /aws/dist
-
-/aws/dist/aws s3 sync /usr/share/nginx/html s3://staging.mlmodelscope.org
+aws s3 sync /usr/share/nginx/html s3://staging.mlmodelscope.org
 
 if [ -n "$CLOUDFRONT_ID" ]; then
-    /aws/dist/aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_ID --path "/*"
+    aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_ID --path "/*"
 fi
 
 echo "S3 Bucket has been updated."
