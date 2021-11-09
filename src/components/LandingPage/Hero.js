2bbee40ef232aa47527a8d8f1f347cd1041a777d
Steve Zazeski <steve@pixotech.com>
2021-11-09 13:11:11 -0600

    Improve the css on the hero, removed duplicate doc button

--- src/components/LandingPage/Hero.js
+++ src/components/LandingPage/Hero.js
@@ -6,18 +6,11 @@ import withSizes from "react-sizes";
 import { withScroll } from "react-fns";
 import Color from "color";
 
-const primaryColor = "#1A263A";
+import './LandingPage.css';
 
-const infoStyle = {
-  position: "absolute",
-  top: "100vh",
-  transform: "translateY(-300%)",
-  width: "100%",
-  height: "40px",
-  color: "white",
-  fontSize: "1.5rem",
-  opacity: "1",
-};
+const primaryColor = "#1A263A";
+const lightPrimary = Color(primaryColor).lighten(0.5).hex();
+const darkerPrimary = Color(primaryColor).darken(0.5).hex();
 
 @withScroll
 @withSizes(({ width }, { breakpoint }) => ({ isMobile: width < breakpoint }))
@@ -36,29 +29,10 @@ class Hero extends Component {
 
 	docslocation = window.location.protocol + "//" + docslocation;
 
-    const lightPrimary = Color(primaryColor)
-      .lighten(0.5)
-      .hex();
-    const darkerPrimary = Color(primaryColor)
-      .darken(0.5)
-      .hex();
+
     const { isMobile } = this.props;
     return (
-      <div
-        style={{
-          // background: "linear-gradient(180deg, rgba(25,38,58,1) 60%, rgba(255,255,255,0.8) 80%)",
-          background: primaryColor,
-          minHeight: "60vh",
-          width: "100%",
-          color: "white",
-          fontWeight: "200",
-          backgroundSize: "100%",
-          paddingTop: "100px",
-          paddingBottom: "3rem",
-          textAlign: "center",
-          paddingLeft: isMobile ? "50px" : "150px",
-        }}
-      >
+      <div class="Hero" style={{paddingLeft: isMobile ? "50px" : "150px",}}>
         <QueueAnim type={isMobile ? "bottom" : "left"} delay={300}>
           <Row type="flex" justify="start" style={{ paddingTop: "5vh" }}>
             <Col
@@ -76,73 +50,13 @@ class Hero extends Component {
             </Col>
           </Row>
           <Row type="flex" justify="start" gutter={16} style={{ marginTop: "3rem" }}>
-            <Col
-              lg={{ span: 4 }}
-              xs={{ span: 20 }}
-              style={{
-                fontSize: isMobile ? "1rem" : "1.5rem",
-                fontWeight: "bold",
-                backgroundColor: lightPrimary,
-                borderColor: lightPrimary,
-                borderStyle: "dashed",
-                marginLeft: "10px",
-                padding: "10px",
-              }}
-            >
-              <a
-                href={docslocation}
-                style={{
-                  color: "white",
-                  margin: "auto",
-                  verticalAlign: "middle",
-                  textAlign: "center",
-                }}
-              >
-                Learn More
-              </a>
+            <Col lg={{ span: 4 }} xs={{ span: 20 }}>
+              <a href={docslocation} className="hero_button">Learn More</a>
             </Col>
             {isMobile ? null : (
-              <React.Fragment>
-                <Col
-                  lg={{ span: 4 }}
-                  xs={{ span: 20 }}
-                  style={{
-                    fontSize: "1.5rem",
-                    fontWeight: "bold",
-                    backgroundColor: lightPrimary,
-                    borderColor: lightPrimary,
-                    borderStyle: "dashed",
-                    marginLeft: "10px",
-                    padding: "10px",
-                  }}
-                >
+                <Col lg={{ span: 4 }} xs={{ span: 20 }}>
+                  <a href="/playground" className="hero_button">Playground</a>
                 </Col>
-                <Col
-                  lg={{ span: 4 }}
-                  xs={{ span: 20 }}
-                  style={{
-                    fontSize: "1.5rem",
-                    fontWeight: "bold",
-                    backgroundColor: lightPrimary,
-                    borderColor: lightPrimary,
-                    borderStyle: "dashed",
-                    marginLeft: "10px",
-                    padding: "10px",
-                  }}
-                >
-                  <a
-                    href="/playground"
-                    style={{
-                      color: "white",
-                      margin: "auto",
-                      verticalAlign: "middle",
-                      textAlign: "center",
-                    }}
-                  >
-                    Playground
-                  </a>
-                </Col>
-              </React.Fragment>
             )}
           </Row>
         </QueueAnim>
