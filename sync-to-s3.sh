3d3525d35b43e7f65031f9ef91b8bcf1e1bcdb68
Steve Zazeski <steve@pixotech.com>
2021-11-12 14:16:45 -0600

    Improve sync command

--- sync-to-s3.sh
+++ sync-to-s3.sh
@@ -6,14 +6,17 @@ echo "sync-to-s3.sh - - - - "
 if [ -z ${AWS_ACCESS_KEY_ID} ]; then echo "Required environment var AWS_ACCESS_KEY_ID not set"; exit 1; fi
 if [ -z ${AWS_SECRET_ACCESS_KEY} ]; then echo "Required environment var AWS_SECRET_ACCESS_KEY not set"; exit 1; fi
 if [ -z ${AWS_DEFAULT_REGION} ]; then echo "Required environment var AWS_DEFAULT_REGION not set"; exit 1; fi
+if [ -z ${S3_BUCKETNAME} ]; then echo "Required environment var S3_BUCKETNAME not set"; exit 1; fi
 
 # download awscli if not already
 if [ -z `stat ./aws/dist/aws` ]; then
-    echo "aws cli is not available, downloading one now"
-    apt-get update && apt-get install awscli -y -qq
+    echo "aws cli is not available, apt-get installing one now"
+    apt-get update > /dev/null && apt-get install awscli -y > /dev/null
+    echo "got awscli."
 fi
 
-aws s3 sync /usr/share/nginx/html s3://staging.mlmodelscope.org
+
+aws s3 sync /usr/share/nginx/html s3://${S3_BUCKETNAME}
 
 if [ -n "$CLOUDFRONT_ID" ]; then
     aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_ID --path "/*"
