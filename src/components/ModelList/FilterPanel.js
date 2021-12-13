d7d6b3d112d5dd22c8132cc59e123a5dec090d80
Anne <annem@pixotech.com>
2021-12-13 16:48:09 -0600

    Framework tags change color on click

--- src/components/ModelList/FilterPanel.js
+++ src/components/ModelList/FilterPanel.js
@@ -1,15 +1,23 @@
 import React, { Component } from "react";
+import "./FilterPanel.css"
 
 export default class FilterPanel extends Component{
   render() {
     return(
       <div style={{background: "gainsboro", padding: "12px"}}>
         <p>Framework</p>
-        <button>Jax</button>
-        <button>MXNet</button>
-        <button>PyTorch</button>
-        <button>TensorFlow</button>
+        <button className="ActiveTag" onClick={OnSelectFramework}>Any</button>
+        <button className="InactiveTag" onClick={OnSelectFramework}>Onnxruntime</button>
+        <button className="InactiveTag" onClick={OnSelectFramework}>MXNet</button>
+        <button className="InactiveTag" onClick={OnSelectFramework}>PyTorch</button>
+        <button className="InactiveTag" onClick={OnSelectFramework}>TensorFlow</button>
       </div>
     );
   }
+}
+
+function OnSelectFramework(e){
+  var target = e.target;
+  var newClass = target.className == "ActiveTag"?"InactiveTag":"ActiveTag";
+  target.className = newClass;
 }
