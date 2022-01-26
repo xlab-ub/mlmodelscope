12b42d6af2318985a1c07308b064bd71d8956d5f
Anne <annem@pixotech.com>
2022-01-26 13:55:16 -0600

    Use task enum to display task information on model detail page

--- src/components/ModelDetailPage/ModelDetailHeader.js
+++ src/components/ModelDetailPage/ModelDetailHeader.js
@@ -1,18 +1,20 @@
 import React, { Component } from "react";
+import Task from "../../helpers/Task";
 
 export default class ModelDetailHeader extends Component{
   render(){
     const model = this.props.model;
     if(!!model){
+      const task = Task[model.output.type];
+      console.log(task);
       return(
         <div>
           <p>Modified: N/A</p>
           <p>{model.name}</p>
           <p>{model.description}</p>
           <div className="ModelDetailHeader__task-box">
-            <label>Task: </label>
-            <p>[task name here]</p>
-            <p>[task description here]</p>
+            <p>Task: {task.name}</p>
+            <p>{task.description}</p>
           </div>
           <div className="ModelDetailHeader__advanced-details-box">
             <p>Advanced Details</p>
