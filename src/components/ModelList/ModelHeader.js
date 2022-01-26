9863401d5b52e632dafd313507a282f81504d9a8
Anne <annem@pixotech.com>
2022-01-26 09:26:47 -0600

    Add description to model list header

--- src/components/ModelList/ModelHeader.js
+++ src/components/ModelList/ModelHeader.js
@@ -3,7 +3,10 @@ import React, { Component } from "react";
 export default class ModelHeader extends Component{
   render() {
     return(
-      <h1> Models </h1>
+      <div class="ModelHeader__group">
+        <h1 class="ModelHeader__title"> Models </h1>
+        <h2 class="ModelHeader__description"> Experience the power of machine learning by testing and comparing models. </h2>
+      </div>
     );
   }
 }
