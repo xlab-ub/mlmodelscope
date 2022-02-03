a9a1ab60d3b6cb1bd69b3db65555e20bb6925fca
Anne <annem@pixotech.com>
2022-02-03 12:12:46 -0600

    Move _type.scss to abstracts and style ModelCard component

--- src/components/ModelList/ModelCard.jsx
+++ src/components/ModelList/ModelCard.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import Task from "../../helpers/Task";
 
 export default class ModelCard extends Component{
   constructor() {
@@ -24,17 +25,25 @@ export default class ModelCard extends Component{
 
   render() {
     const model = this.props.model;
-    const task = this.taskNameToLabel(model.output.type);
+    let task = Task[model.output.type];
+    if (!task) {
+      task = new Task(model.output.type, "This task has no definition");
+    }
     const modelLink = "/model/" + model.id;
     return(
-      <div className="ModelCardDiv">
-        <div className="ModelTagsBox">
-          <button>{model.framework.name}</button>
-          <button>{task}</button>
+      <div className="model-card">
+        <div className="model-card__tags-box">
+          <p className="model-card__tag-label">Framework:</p>
+          <button className="model-card__tag">{model.framework.name}</button>
+          <p className="model-card__tag-label">Task:</p>
+          <button className="model-card__tag">{task.name}</button>
         </div>
-        <a className="ModelCard__model-name" href={modelLink}>{model.name}</a>
-        <p className="ModelVersion">Version {model.version}</p>
-        <p className="ModelDescription">{model.description}</p>
+        <a className="model-card__model-name" href={modelLink}>{model.name} Version {model.version}</a>
+        <p className="model-card__model-desc">{model.description}</p>
+        <div className="model-card__actions-box">
+          <a className="model-card__try-model-button" href={modelLink}>Try this model</a>
+          <a className="model-card__learn-more-button" href={modelLink}>Learn more</a>
+        </div>
       </div>
     )
   }
