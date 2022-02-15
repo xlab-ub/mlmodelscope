e587efe1a6d864ce3377092462039f92c51cf8a3
Ben Young <ben@pixotech.com>
2022-02-15 08:16:42 -0600

    WIP: Example tests with Enzyme

--- src/components/Paginator/pagination.js
+++ src/components/Paginator/pagination.js
@@ -1,22 +1,35 @@
 import React, { Component } from "react";
 
 export default function withPagination(WrappedComponent) {
-  return class extends Component {
+  class Paginated extends Component {
+    static defaultProps = {
+      data: [],
+      pageCount: 1,
+      selectedPage: 1
+    }
+
     constructor(props) {
       super(props);
       this.selectPage = this.selectPage.bind(this);
-      this.state = {
-        data: [],
-        pageCount: 0,
-        selectedPage: 0
-      };
+      this.state = this.paginateData();
     }
 
     componentDidUpdate(prevProps: Readonly<P>, prevState: Readonly<S>, snapshot: SS) {
       if (prevProps.data !== this.props.data) {
-        this.setState({
-          data: this.props.data
-        });
+        this.setState(this.paginateData());
+      }
+    }
+
+    paginateData() {
+      const start = 12 * (this.props.selectedPage - 1);
+      const end = 12 * this.props.selectedPage;
+      const data = this.props.data.slice(start, end);
+      const pageCount = parseInt(1 + this.props.data.length / 12);
+
+      return {
+        data,
+        pageCount,
+        selectedPage: 1
       }
     }
 
@@ -30,4 +43,6 @@ export default function withPagination(WrappedComponent) {
       return <WrappedComponent data={this.state.data} pageCount={this.state.pageCount} selectedPage={this.state.selectedPage} selectPage={this.selectPage} {...this.props} />;
     }
   }
+
+  return Paginated;
 }
