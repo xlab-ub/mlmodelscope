e3e21ffe4d08db25e396cbbc0c5aab1da1ba2e22
Anne <annem@pixotech.com>
2022-02-17 16:22:29 -0600

    Show fake sort options underneath "sort by" button when it is focused
    C3SR-125

--- src/components/Buttons/SortButton.jsx
+++ src/components/Buttons/SortButton.jsx
@@ -2,13 +2,36 @@ import React, {Component} from "react";
 import {ReactComponent as SortBars} from "../../resources/icons/sort-bars.svg";
 
 export default class SortButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      optionsHidden: true,
+    }
+  }
+
+  showOptions = () => {
+    this.setState({
+      optionsHidden: false
+    });
+  }
+
+  hideOptions = () => {
+    this.setState({
+      optionsHidden: true,
+    });
+  }
+
   render() {
     return (
       <div className="sort-button">
-        <button className="sort-button__button">
+        <button className="sort-button__button" onFocus={this.showOptions} onBlur={this.hideOptions}>
           <SortBars className="sort-button__icon"/>
           Sort by
         </button>
+        <div className="sort-button__option-box" hidden={this.state.optionsHidden}>
+          <button className="sort-button__option">Ascending</button>
+          <button className="sort-button__option">Descending</button>
+        </div>
       </div>
     );
   }
