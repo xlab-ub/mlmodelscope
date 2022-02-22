00c9f1bf84a6420b752e5d609a8c7ae24b43569a
Ben Young <ben@pixotech.com>
2022-02-22 14:31:46 -0600

    Implement basic functionality of SampleInputsTab

--- src/components/Experiment/QuickInput/SampleInputsTab.js
+++ src/components/Experiment/QuickInput/SampleInputsTab.js
@@ -1,10 +1,48 @@
 import React, {Component} from 'react';
 
 export default class SampleInputsTab extends Component {
+  static defaultProps = {
+    inputSelected: undefined,
+    sampleInputs: []
+  }
+
+  constructor(props) {
+    super(props);
+    this.classname = "sample-inputs";
+    this.state = {
+      selectedIndex: -1
+    }
+  }
+
   render() {
     return (
       <div className={this.classname}>
+        <div className={`${this.classname}__title`}>Select one of our sample images to run through the model</div>
+        <div className={`${this.classname}__list`}>
+          {this.props.sampleInputs.map(this.makeSampleInput)}
+        </div>
       </div>
     );
+  }
+
+  makeSampleInput = (url, index) => {
+    let classes = `${this.classname}__input`;
+    if (this.state.selectedIndex === index)
+      classes = `${classes} ${this.classname}__input--selected`;
+
+    return (
+      <div key={index} className={classes}>
+        <img src={url} onClick={() => this.selectInput(index)} />
+      </div>
+    )
+  };
+
+  selectInput = (index) => {
+    this.setState({
+      selectedIndex: index
+    });
+
+    if (typeof(this.props.inputSelected) === 'function')
+      this.props.inputSelected(this.props.sampleInputs[index]);
   }
 }
---
+++ src/components/Experiment/QuickInput/SampleInputsTab.test.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import expect from 'expect';
+import {shallow} from 'enzyme';
+import SampleInputsTab from "./SampleInputsTab";
+
+const SampleInputs = [
+  'https://example.com/sample1.jpg',
+  'https://example.com/sample2.jpg',
+  'https://example.com/sample2.jpg',
+];
+
+describe('Sample Inputs Tab', () => {
+  describe('Renders an empty input list', () => {
+    it('with a title and no sample inputs', () => {
+      const wrapper = shallow(<SampleInputsTab/>);
+      const tab = wrapper.find('.sample-inputs');
+      const list = wrapper.find('.sample-inputs').find('.sample-inputs__list');
+
+      expect(tab.length).toBe(1);
+      expect(tab.childAt(0).prop('className')).toBe('sample-inputs__title');
+      expect(tab.childAt(0).text()).toBe('Select one of our sample images to run through the model');
+      expect(list.length).toBe(1);
+      expect(list.children().length).toBe(0);
+    });
+  });
+
+  describe('Or a list of sample inputs', () => {
+    let wrapper;
+
+    beforeEach(() => {
+      wrapper = shallow(<SampleInputsTab sampleInputs={SampleInputs} />);
+    });
+
+    it('from a provided array of sample inputs', () => {
+      const list = wrapper.find('.sample-inputs__list');
+      const inputs = list.find('.sample-inputs__input');
+
+      expect(inputs.length).toBe(3);
+      expect(inputs.at(0).find('img').prop('src')).toBe(SampleInputs[0]);
+      expect(inputs.at(1).find('img').prop('src')).toBe(SampleInputs[1]);
+      expect(inputs.at(2).find('img').prop('src')).toBe(SampleInputs[2]);
+    });
+
+    it('starting with no input selected', () => {
+      expect(wrapper.find('.sample-inputs__input--selected').length).toBe(0);
+    });
+
+    it('allowing selection by clicking by clicking the input img', () => {
+      const image = wrapper.find('.sample-inputs__input').at(2).find('img');
+      image.simulate('click');
+      const selected = wrapper.find('.sample-inputs__input--selected');
+
+      expect(selected.length).toBe(1);
+      expect(selected.find('img').prop('src')).toBe(SampleInputs[2]);
+    });
+
+    it('only allowing single selection', () => {
+      const image = wrapper.find('.sample-inputs__input').at(1).find('img');
+      image.simulate('click');
+      const selected = wrapper.find('.sample-inputs__input--selected');
+
+      expect(selected.length).toBe(1);
+    });
+
+    it('calling the inputSelected callback when an input is selected', () => {
+      const inputSelected = jest.fn();
+      wrapper = shallow(<SampleInputsTab sampleInputs={SampleInputs} inputSelected={inputSelected} />);
+      const image = wrapper.find('.sample-inputs__input').at(1).find('img');
+      image.simulate('click');
+
+      expect(inputSelected.mock.calls.length).toBe(1);
+      expect(inputSelected.mock.calls[0][0]).toBe(SampleInputs[1]);
+    });
+  });
+});
