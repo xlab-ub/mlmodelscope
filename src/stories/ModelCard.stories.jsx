64af079eb42018b0ac88d0e48b0c09d2af07b62f
Anne <annem@pixotech.com>
2022-02-22 14:19:38 -0600

    Display machine tags on model cards

--- src/stories/ModelCard.stories.jsx
+++ src/stories/ModelCard.stories.jsx
@@ -86,3 +86,49 @@ LongName.args = {
     "version": "1.0"
   }
 };
+
+export const MultipleMachines = Template.bind({});
+MultipleMachines.args = {
+  key: 'extra-machine-model',
+  model: {
+    "id": 13,
+    "attributes": {
+      "Top1": "78.56",
+      "Top5": "94.43",
+      "kind": "CNN",
+      "manifest_author": "Cheng Li",
+      "training_dataset": "ImageNet"
+    },
+    "description": "This is a made up example of a model that supports multiple machines\n",
+    "model": {
+      "graph_checksum": "",
+      "graph_path": "model-symbol.json",
+      "weights_checksum": "",
+      "weights_path": "model-0000.params"
+    },
+    "framework": {
+      "id": 1,
+      "name": "MXNet",
+      "version": "1.7.0",
+      "architectures": [
+        {
+          "name": "amd64"
+        },
+        {
+          "name": "ILLIAC"
+        }
+      ]
+    },
+    "input": {
+      "description": "",
+      "type": ""
+    },
+    "license": "unrestricted",
+    "name": "Fakenet22",
+    "output": {
+      "description": "the output label",
+      "type": "classification"
+    },
+    "version": "2.22"
+  }
+};
