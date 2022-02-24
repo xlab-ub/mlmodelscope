717d2050833e0d4631a675887ee9cff725954bc3
Anne <annem@pixotech.com>
2022-02-24 11:16:37 -0600

    Restructure model details into 4 columns

--- src/components/ModelDetailPage/ModelDetailHeader.js
+++ src/components/ModelDetailPage/ModelDetailHeader.js
@@ -20,32 +20,25 @@ export default class ModelDetailHeader extends Component{
             <p className="model-detail-header__model-name">{model.name} Version {model.version}</p>
             <p className="model-detail-header__model-description">{model.description}</p>
           </div>
-          <div className="model-detail-header__task-box">
-            <dl>
-              <div className="model-detail-header__detail-container">
-                <dt className="model-detail-header__task-label">Task:</dt>
-                <dd className="model-detail-header__detail">{task.name}</dd>
-              </div>
-            </dl>
-            <p>{task.description}</p>
-          </div>
-          <div className="model-detail-header__advanced-details-box">
-            <p className="model-detail-header__advanced-details-label">Advanced Details</p>
-            <dl>
-              <div className="model-detail-header__detail-container">
-                <dt className="model-detail-header__detail-label">Framework:</dt>
-                <dd className="model-detail-header__detail">{model.framework.name}</dd>
-              </div>
-              <div className="model-detail-header__detail-container">
-                <dt className="model-detail-header__detail-label">Trained on:</dt>
-                <dd className="model-detail-header__detail">{model.attributes.training_dataset}</dd>
-              </div>
-              <div className="model-detail-header__detail-container">
-                <dt className="model-detail-header__detail-label">Machines:</dt>
-                <dd className="model-detail-header__detail">N/A</dd>
-              </div>
-            </dl>
-          </div>
+          <dl className="model-detail-header__data-box">
+            <div className="model-detail-header__task-box">
+              <dt className="model-detail-header__task-label">Task:</dt>
+              <dd className="model-detail-header__detail">{task.name}</dd>
+              <dd>{task.description}</dd>
+            </div>
+            <div className="model-detail-header__framework-box">
+              <dt className="model-detail-header__detail-label">Framework:</dt>
+              <dd className="model-detail-header__detail">{model.framework.name}</dd>
+            </div>
+            <div className="model-detail-header__machines-box">
+              <dt className="model-detail-header__detail-label">Machines:</dt>
+              <dd className="model-detail-header__detail">N/A</dd>
+            </div>
+            <div className="model-detail-header__training-set-box">
+              <dt className="model-detail-header__detail-label">Trained on:</dt>
+              <dd className="model-detail-header__detail">{model.attributes.training_dataset}</dd>
+            </div>
+          </dl>
         </div>
       );
     }
