013e3ee6ee40e727ff2e092fb85f046ce3feb9ae
Anne <annem@pixotech.com>
2022-03-02 16:55:08 -0600

    Add barebones overview component and story

--- src/components/ExperimentDetails/ExperimentDetailPage.jsx
+++ src/components/ExperimentDetails/ExperimentDetailPage.jsx
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import ExperimentDetailHeader from "./ExperimentDetailHeader";
+import ExperimentOverview from "./ExperimentOverview";
 
 export default class ExperimentDetailPage extends Component {
   render() {
     return (
       <div>
         <ExperimentDetailHeader />
+        <ExperimentOverview />
       </div>
     );
   }
