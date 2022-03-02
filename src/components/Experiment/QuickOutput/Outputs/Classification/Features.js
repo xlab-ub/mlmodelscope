876a9c63e59c3c6f22fff7ba390c64b1840d1bcb
Ben Young <ben@pixotech.com>
2022-03-02 13:58:49 -0600

    Add Prediction component

---
+++ src/components/Experiment/QuickOutput/Outputs/Classification/Features.js
@@ -0,0 +1,40 @@
+const TestFeatures = [
+  {
+    "classification": {
+      "index": 933,
+      "label": "n07697313 cheeseburger"
+    },
+    "id": "61afb91c7cc38300018b8a74",
+    "probability": 0.64689136,
+    "type": "CLASSIFICATION"
+  },
+  {
+    "classification": {
+      "index": 934,
+      "label": "n07697537 hotdog, hot dog, red hot"
+    },
+    "id": "61afb91c7cc38300018b8a75",
+    "probability": 0.08022722,
+    "type": "CLASSIFICATION"
+  },
+  {
+    "classification": {
+      "index": 962,
+      "label": "n07871810 meat loaf, meatloaf"
+    },
+    "id": "61afb91c7cc38300018b8a91",
+    "probability": 0.078421086,
+    "type": "CLASSIFICATION"
+  },
+  {
+    "classification": {
+      "index": 924,
+      "label": "n07583066 guacamole"
+    },
+    "id": "61afb91c7cc38300018b8a6b",
+    "probability": 0.038271908,
+    "type": "CLASSIFICATION"
+  }
+];
+
+export default TestFeatures;
---
+++ src/components/Experiment/QuickOutput/Outputs/Classification/Prediction.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import expect from 'expect';
+import {shallow} from 'enzyme';
+import Prediction from "./Prediction";
+import TestFeatures from "./Features";
+import formatProbability from "./ProbabilityFormatter";
+
+describe('Prediction component', () => {
+  describe('Reners', () => {
+    let wrapper;
+
+    beforeEach(() => {
+      wrapper = shallow(<Prediction feature={TestFeatures[0]} />);
+    });
+
+    it('with a container div', () => {
+      expect(wrapper.at(0).prop('className')).toBe('prediction');
+    });
+
+    it('with a prediction label', () => {
+      const label = wrapper.childAt(0);
+
+      expect(label.prop('className')).toBe('prediction__label');
+      expect(label.text()).toBe(TestFeatures[0].classification.label);
+    });
+
+    it('with a prediction probability', () => {
+      const label = wrapper.childAt(1);
+
+      expect(label.prop('className')).toBe('prediction__probability');
+      expect(label.text()).toBe(formatProbability(TestFeatures[0].probability));
+    });
+  });
+});
