1e062a068f7eb309a43126ce4aa427e9cf92b012
Anne <annem@pixotech.com>
2022-03-03 12:02:47 -0600

    Add placeholder input image and task to overview, split detail page into columns

--- src/components/ExperimentDetails/ExperimentDetailPage.jsx
+++ src/components/ExperimentDetails/ExperimentDetailPage.jsx
@@ -1,13 +1,21 @@
 import React, {Component} from 'react';
 import ExperimentDetailHeader from "./ExperimentDetailHeader";
 import ExperimentOverview from "./ExperimentOverview";
+import "./ExperimentDetailPage.scss";
 
 export default class ExperimentDetailPage extends Component {
   render() {
     return (
-      <div>
+      <div className="experiment-detail-page">
         <ExperimentDetailHeader />
-        <ExperimentOverview />
+        <div className="experiment-detail-page__content">
+          <div className="experiment-detail-page__overview-section">
+            <ExperimentOverview />
+          </div>
+          <div className="experiment-detail-page__trials-section">
+            <p>Trial stuff goes here</p>
+          </div>
+        </div>
       </div>
     );
   }
