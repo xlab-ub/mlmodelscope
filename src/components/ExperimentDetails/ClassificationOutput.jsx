4bb105ddf7b6382205381ff46aad5d571433957c
Anne <annem@pixotech.com>
2022-03-07 13:59:43 -0600

    Add to and partially style output components
    C3SR-155

--- src/components/ExperimentDetails/ClassificationOutput.jsx
+++ src/components/ExperimentDetails/ClassificationOutput.jsx
@@ -1,11 +1,21 @@
 import React, {Component} from 'react';
 
 export default class ClassificationOutput extends Component {
+  resultsFromInclusiveRange = (x, y) => {
+    let resultsInRange = this.props.results.slice(x, y+1);
+  }
+
   render() {
+    let results = this.props.results;
     return (
-      <div>
-        <p>Result 1</p>
-        <p>Result 2</p>
+      <div className="classification-output">
+        <p className="classification-output__header">Output:</p>
+        <dl className="classifcation-output__results">
+          <div className="classification-output__result -top-result">
+            <dt>{results[0].prediction}</dt>
+            <dd>{results[0].confidence}</dd>
+          </div>
+        </dl>
       </div>
     )
   }
--- src/components/ExperimentDetails/TrialOutputWrapper.jsx
+++ src/components/ExperimentDetails/TrialOutputWrapper.jsx
@@ -1,12 +1,42 @@
 import React, {Component} from 'react';
 import ClassificationOutput from "./ClassificationOutput";
+import ModelTag from "../Common/ModelTag";
 
 export default class TrialOutputWrapper extends Component {
   render() {
+    let model = this.props.trial.model;
+
+    let machineTagKey = 0;
+    let machineTags = <dd>N/A</dd>
+    if (!!model.framework.architectures) {
+      machineTags = model.framework.architectures.map(machine => (
+        <dd className="trial-output-wrapper__model-tag">
+          <ModelTag key={machineTagKey++} type="machine" content={machine.name}/>
+        </dd>));
+    }
+
     return (
-      <div>
-        <p>Trial output here</p>
-        <ClassificationOutput />
+      <div className="trial-output-wrapper">
+        <div className="trial-output-wrapper__title-box">
+          <dl>
+            <dt className="trial-output-wrapper__model-label">Model:</dt>
+            <dd className="trial-output-wrapper__model-name">{model.name}</dd>
+          </dl>
+        </div>
+        <div className="trial-output-wrapper__content-box">
+          <div className="trial-output-wrapper__info-row">
+            <dl className="trial-output-wrapper__model-details">
+              <dt className="trial-output-wrapper__detail-label">Framework:</dt>
+              <dd className="trial-output-wrapper__model-tag"><ModelTag type="framework" content={model.framework.name} /></dd>
+              <dt className="trial-output-wrapper__detail-label">Machines:</dt>
+              {machineTags}
+            </dl>
+            <div className="trial-output-wrapper__link-box">
+              <a className="trial-output-wrapper__link" href="">Advanced Output Analysis</a>
+            </div>
+          </div>
+          <ClassificationOutput results={this.props.trial.results} />
+        </div>
       </div>
     )
   }
