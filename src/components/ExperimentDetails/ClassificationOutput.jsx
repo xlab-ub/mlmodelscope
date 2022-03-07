73e080e7af2231cd3f9a54882029eefd45177058
Anne <annem@pixotech.com>
2022-03-07 10:46:43 -0600

    Create barebones trial output components and story
    C3SR-155

---
+++ src/components/ExperimentDetails/ClassificationOutput.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class ClassificationOutput extends Component {
+  render() {
+    return (
+      <div>
+        <p>Result 1</p>
+        <p>Result 2</p>
+      </div>
+    )
+  }
+}
