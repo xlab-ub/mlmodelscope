c424c37f66c0fac203b7d9140dd0825fbb906bef
Anne <annem@pixotech.com>
2022-03-07 10:45:30 -0600

    Add grid gap to experiment detail page and update storybook to send experiment data

--- src/components/ExperimentDetails/ExperimentDetailPage.stories.js
+++ src/components/ExperimentDetails/ExperimentDetailPage.stories.js
@@ -10,4 +10,77 @@ export default {
 const Template = (args) => <ExperimentDetailPage {...args} />;
 
 export const Standard = Template.bind({});
-Standard.args = {};
+Standard.args = {
+  experiment: {
+    id: 0,
+    task: "classification",
+    trials: [
+      {
+        model: {
+          name: "TorchVision+ResNet18",
+          framework: {
+            name: "PyTorch",
+            architectures: [
+              {name: "amd64"}
+            ]
+          }
+        },
+        results: [
+          {
+            prediction: "Cat",
+            confidence: 96,
+          },
+          {
+            prediction: "Dog",
+            confidence: 2,
+          },
+          {
+            prediction: "Mouse",
+            confidence: 1,
+          },
+          {
+            prediction: "Tiger shark",
+            confidence: 0.5,
+          },
+          {
+            prediction: "Chicken",
+            confidence: 0.5,
+          }
+        ]
+      },
+      {
+        model: {
+          name: "AI_Matrix_ResNet152",
+          framework: {
+            name: "PyTorch",
+            architectures: [
+              {name: "amd64"}
+            ]
+          }
+        },
+        results: [
+          {
+            prediction: "Cat",
+            confidence: 97,
+          },
+          {
+            prediction: "Chicken",
+            confidence: 1,
+          },
+          {
+            prediction: "Mouse",
+            confidence: 1,
+          },
+          {
+            prediction: "Tiger shark",
+            confidence: 0.5,
+          },
+          {
+            prediction: "Dog",
+            confidence: 0.5,
+          }
+        ]
+      }
+    ]
+  }
+};
