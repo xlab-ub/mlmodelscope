9e487eadd846300f95a63adaf5bb68bb566b3429
Ben Young <ben@pixotech.com>
2022-03-07 15:10:50 -0600

    Update ClassificationOutput structure and toggle the expand button label

--- src/components/Experiment/QuickOutput/Outputs/Classification/ClassificationOutput.js
+++ src/components/Experiment/QuickOutput/Outputs/Classification/ClassificationOutput.js
@@ -30,21 +30,25 @@ export default class ClassificationOutput extends BEMComponent {
       <div className={this.block()}>
         <h3 className={this.element('title')}>Output</h3>
         <div className={this.element('subtitle')}>What this model thinks the image is.</div>
-        <TopPrediction feature={this.props.features[0]} />
         <div className={this.element('predictions')}>
+          <TopPrediction feature={this.props.features[0]} />
           {this.props.features.slice(1, 3).map(this.makePrediction)}
 
           <div className={this.element('prediction-overflow')}>
             {this.props.features.slice(3).map(this.makePrediction)}
           </div>
-          <a className={this.element('expand')} onClick={this.expandClicked} />
         </div>
+        <button className={this.element('expand')} onClick={this.expandClicked}>{this.makeExpanderLabel()}</button>
       </div>
     );
   }
 
   makePrediction = (feature, index) => {
     return <Prediction key={index} feature={feature} />
+  }
+
+  makeExpanderLabel = () => {
+    return `${this.state.predictionsExpanded ? 'Hide' : 'Show'} all predictions`;
   }
 
   expandClicked = () => {
--- src/components/Experiment/QuickOutput/Outputs/Classification/ClassificationOutput.test.js
+++ src/components/Experiment/QuickOutput/Outputs/Classification/ClassificationOutput.test.js
@@ -31,26 +31,28 @@ describe('Classification Output Component', () => {
       expect(wrapper.childAt(1).text()).toBe('What this model thinks the image is.');
     });
 
-    describe('with a top prediction component', () => {
-      it('that has been passed the first prediction', () => {
-        expect(wrapper.childAt(2).type()).toBe(TopPrediction);
-        expect(wrapper.childAt(2).prop('feature')).toBe(TestFeatures[0]);
-      });
-    });
+    describe('with a list of predictions', () => {
+      describe('beginning with the top prediction component', () => {
+        it('that has been passed the first prediction', () => {
+          const topPrediction = wrapper.childAt(2).childAt(0);
 
-    describe('with a list of additional predictions', () => {
+          expect(topPrediction.type()).toBe(TopPrediction);
+          expect(topPrediction.prop('feature')).toBe(TestFeatures[0]);
+        });
+      });
+
       it('that shows the second two predictions by default', () => {
-        const predictions = wrapper.childAt(3);
+        const predictions = wrapper.childAt(2);
 
         expect(predictions.prop('className')).toBe('classification-output__predictions');
-        expect(predictions.childAt(0).type()).toBe(Prediction);
-        expect(predictions.childAt(0).prop('feature')).toBe(TestFeatures[1]);
         expect(predictions.childAt(1).type()).toBe(Prediction);
-        expect(predictions.childAt(1).prop('feature')).toBe(TestFeatures[2]);
+        expect(predictions.childAt(1).prop('feature')).toBe(TestFeatures[1]);
+        expect(predictions.childAt(2).type()).toBe(Prediction);
+        expect(predictions.childAt(2).prop('feature')).toBe(TestFeatures[2]);
       });
 
       it('that hides the fourth prediction and beyond in an expandable container', () => {
-        const additional = wrapper.childAt(3).childAt(2);
+        const additional = wrapper.childAt(2).childAt(3);
 
         expect(additional.prop('className')).toBe('classification-output__prediction-overflow classification-output__prediction-overflow--collapsed');
         expect(additional.children.length).toBe(1);
@@ -59,18 +61,20 @@ describe('Classification Output Component', () => {
       });
 
       it('that has a button to expand the expandable container', () => {
-        const expander = wrapper.childAt(3).childAt(3);
+        const expander = wrapper.childAt(3);
 
-        expect(expander.type()).toBe('a');
+        expect(expander.type()).toBe('button');
+        expect(expander.text()).toBe('Show all predictions');
         expect(expander.prop('className')).toBe('classification-output__expand classification-output__expand--collapsed');
       });
 
       it('where clicking the expand button expands the container', () => {
-        wrapper.childAt(3).childAt(3).simulate('click');
+        wrapper.childAt(3).simulate('click');
         const expander = wrapper.find('.classification-output__expand');
         const overflow = wrapper.find('.classification-output__prediction-overflow');
 
         expect(expander.prop('className')).toBe('classification-output__expand');
+        expect(expander.text()).toBe('Hide all predictions');
         expect(overflow.prop('className')).toBe('classification-output__prediction-overflow');
       });
     });
