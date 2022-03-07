eafc45180bfec0cfeacee430e5555f58b40641d8
Anne <annem@pixotech.com>
2022-03-07 15:32:56 -0600

    Refine presentation of trial results
    C3SR-155

--- src/components/ExperimentDetails/ClassificationOutput.jsx
+++ src/components/ExperimentDetails/ClassificationOutput.jsx
@@ -1,8 +1,24 @@
 import React, {Component} from 'react';
 
 export default class ClassificationOutput extends Component {
-  resultsFromInclusiveRange = (x, y) => {
-    let resultsInRange = this.props.results.slice(x, y+1);
+  predictionsFromInclusiveRange = (x, y) => {
+    let predictionsInRange = this.props.results.responses[0].features.slice(x, y+1);
+    let predictionKey = 0;
+    return predictionsInRange.map(prediction => (
+      <div key={predictionKey++} className="classification-output__result">
+        <dt>{this.makeReadableLabel(prediction.classification.label)}</dt>
+        <dd>{this.makeProbabilityStringFromDecimal(prediction.probability)}</dd>
+      </div>
+    ));
+  }
+
+  makeReadableLabel = (originalLabel) => {
+    let firstSpaceLoc = originalLabel.indexOf(" ");
+    return originalLabel.slice(firstSpaceLoc + 1);
+  }
+
+  makeProbabilityStringFromDecimal = (decimalProbability) => {
+    return `${Math.round(decimalProbability * 100)}%`;
   }
 
   render() {
@@ -11,10 +27,11 @@ export default class ClassificationOutput extends Component {
       <div className="classification-output">
         <p className="classification-output__header">Output:</p>
         <dl className="classifcation-output__results">
-          <div className="classification-output__result -top-result">
-            <dt>{predictions[0].classification.label}</dt>
-            <dd>{predictions[0].probability}</dd>
+          <div className="classification-output__top-result">
+            <dt>{this.makeReadableLabel(predictions[0].classification.label)}</dt>
+            <dd>{this.makeProbabilityStringFromDecimal(predictions[0].probability)}</dd>
           </div>
+          {this.predictionsFromInclusiveRange(1, 2)}
         </dl>
       </div>
     )
