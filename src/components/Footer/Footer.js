668160dd44ae30c741ee3a74249fd68be5dd8dec
Alex <alex@pixotech.com>
2022-03-18 13:16:43 -0500

    Finish up the footer.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
 import "./Footer.scss"
 
-import C3SRLogo from "../../resources/logo/logo-c3sr.png";
+import C3SRLogo from "../../resources/logo/C3SR_white.png";
+import IllinoisLogo from "../../resources/logo/uiuc_white.png";
+import BuffaloLogo from "../../resources/logo/Buffalo_white.png";
 
 export default class Footer extends Component {
   render() {
@@ -9,7 +11,13 @@ export default class Footer extends Component {
       <div className="footer">
         <div className="footer__logos-container">
           <a className="footer__logo-link" href="https://c3sr.com">
-            <img className="footer__logo" src={C3SRLogo} />
+            <img className="footer__logo" src={C3SRLogo} alt="Center for cognitive computing systems research" />
+          </a>
+          <a className="footer__logo-link" href="https://illinois.edu">
+            <img className="footer__logo" src={IllinoisLogo} alt="University of Illinois" />
+          </a>
+          <a className="footer__logo-link" href="https://www.buffalo.edu/">
+            <img className="footer__buffalo-logo" src={BuffaloLogo} alt="University at Buffalo" />
           </a>
         </div>
       </div>
---
+++ src/components/Footer/Footer.stories.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Footer from "./Footer";
+
+export default {
+  title: 'Layout/Footer',
+  component: Footer,
+};
+
+const Template = (args) => <Footer {...args} />;
+
+export const Default = Template.bind({});
