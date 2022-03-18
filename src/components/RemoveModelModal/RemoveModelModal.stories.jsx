f9f226c269d87c8900d32e022eb91dfb4d596a56
Alex <alex@pixotech.com>
2022-03-18 09:41:38 -0500

    Getting started on the remove model modal.

---
+++ src/components/RemoveModelModal/RemoveModelModal.stories.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import RemoveModelModal from "./RemoveModelModal";
+
+export default {
+  title: 'Modals/Remove Model Modal',
+  component: RemoveModelModal,
+}
+
+const Template = (args) => <RemoveModelModal {...args} />;
+
+export const Default = Template.bind({});
