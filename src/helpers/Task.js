8d06fac9a3364e512b95ebe08b0157eeed957bf5
Alex <alex@pixotech.com>
2022-03-21 10:22:43 -0500

    Minor update to overview description from review.

--- src/helpers/Task.js
+++ src/helpers/Task.js
@@ -1,5 +1,5 @@
 export default class Task {
-  static classification = new Task('Classification', 'This model is trained to recognize a single object in an image');
+  static classification = new Task('Classification', 'These models are trained to recognize a single object in an image');
   static boundingbox = new Task('Object Detection', '[insert object detection description here]');
   static semanticsegment = new Task('Semantic Segmentation', '[insert semantic segmentation description here]');
   static instancesegment = new Task('Instance Segmentation', '[insert instance segmentation description here]');
