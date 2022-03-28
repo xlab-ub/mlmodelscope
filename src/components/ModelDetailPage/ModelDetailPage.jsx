f8593b901bc675c3dbcf9b6b1a64bc502dcbf0a2
Ben Young <ben@pixotech.com>
2022-03-28 16:24:26 -0500

    Pull sample inputs from the staging S3 bucket instead of the Storybook assets
    This is so that ML agents can access the sample input URLs for inference.

--- src/components/ModelDetailPage/ModelDetailPage.jsx
+++ src/components/ModelDetailPage/ModelDetailPage.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import ModelDetailHeader from "./ModelDetailHeader";
 import QuickInput from "../Experiment/QuickInput/QuickInput";
-import birdy from '../../stories/assets/birdy.png'
-import crabby from '../../stories/assets/crabby.png';
-import kitty from '../../stories/assets/kitty.png';
 import QuickOutput from "../Experiment/QuickOutput/QuickOutput";
 import Header from "../Header/Header";
+
+const SampleInputs = [
+  'https://s3.amazonaws.com/uploads.staging.mlmodelscope.org/birdy.png',
+  'https://s3.amazonaws.com/uploads.staging.mlmodelscope.org/kitty.png',
+  'https://s3.amazonaws.com/uploads.staging.mlmodelscope.org/crabby.png'
+]
 
 export default class ModelDetailPage extends Component {
   static defaultProps = {
@@ -26,7 +29,7 @@ export default class ModelDetailPage extends Component {
 
   renderContent = () => {
     if (this.props.trialOutput === undefined) {
-      return (<QuickInput sampleInputs={[kitty, crabby, birdy]} onRunModelClicked={this.props.onRunModelClicked}/>);
+      return (<QuickInput sampleInputs={SampleInputs} onRunModelClicked={this.props.onRunModelClicked}/>);
     } else {
       return (<QuickOutput input={this.props.trialOutput.inputs[0]} features={this.props.trialOutput.results.responses[0].features} onBackClicked={this.props.onBackToModelClicked} compare={this.props.compare} />);
     }
