06bde4ea1e6ac6c330ff52dd2ff25e4342648f9e
andrew <andrew@pixotech.com>
2022-05-10 17:00:34 -0500

    Update task descriptions
    C3SR-240

--- src/components/ModelDetailPage/ModelDetailPage.stories.jsx
+++ src/components/ModelDetailPage/ModelDetailPage.stories.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import ModelDetailPage from "./ModelDetailPage";
 import kitty from "../../stories/assets/kitty.png";
+import {image_enhancement, object_detection, semantic_segmentation} from "../../helpers/TaskIDs";
 
 export default {
   title: 'Models/Model Detail Page',
@@ -9,6 +10,60 @@ export default {
 };
 
 const Template = (args) => <ModelDetailPage {...args} />;
+
+const makeArgs = (type) => ({
+  key: "StandardModelDetailPage",
+  model: {
+    "id": 13,
+    "created_at": "2022-01-10T20:03:19.910198Z",
+    "updated_at": "2022-01-10T20:03:19.910198Z",
+    "attributes": {
+      "Top1": "78.56",
+      "Top5": "94.43",
+      "kind": "CNN",
+      "manifest_author": "Cheng Li",
+      "training_dataset": "ImageNet"
+    },
+    "description": "This is a made up example of a model that supports multiple machines\n",
+    "model": {
+      "graph_checksum": "",
+      "graph_path": "model-symbol.json",
+      "weights_checksum": "",
+      "weights_path": "model-0000.params"
+    },
+    "framework": {
+      "id": 1,
+      "name": "MXNet",
+      "version": "1.7.0",
+      "architectures": [
+        {
+          "name": "amd64"
+        },
+        {
+          "name": "ILLIAC"
+        }
+      ]
+    },
+    "input": {
+      "description": "",
+      "type": ""
+    },
+    "license": "unrestricted",
+    "name": "Fakenet22",
+    "output": {
+      "description": "the output label",
+      "type": type
+    },
+    "version": "2.22",
+    "url": {
+      "citation": "https://github.com",
+      "github": "https://github.com",
+      "link1": "https://pytorch.org/vision/stable/models.html",
+      "link2": "https://pytorch.org/vision/stable/models.html"
+    }
+  }
+})
+
 
 export const Standard = Template.bind({});
 Standard.args = {
@@ -247,3 +302,15 @@ WithPendingTrialOutput.args = {
     "inputs": [kitty],
   }
 }
+
+export const ObjectDetection = Template.bind({});
+ObjectDetection.args = makeArgs(object_detection);
+
+export const SemanticSegmentation = Template.bind({});
+SemanticSegmentation.args = makeArgs(semantic_segmentation);
+
+export const ImageEnhancement = Template.bind({});
+ImageEnhancement.args = makeArgs(image_enhancement);
+
+export const InstanceSegmentation = Template.bind({});
+InstanceSegmentation.args = makeArgs("image_instance_segmentation");
--- src/helpers/Task.js
+++ src/helpers/Task.js
@@ -1,10 +1,10 @@
 export default class Task {
-  static image_classification = new Task('Classification', 'These models are trained to recognize a single object in an image', 'This model is trained to recognize a single object in an image');
+  static image_classification = new Task('Classification', 'Used to recognize a single object in an image and can help you answer what is in the image', 'Used to recognize a single object in an image and can help you answer what is in the image');
   static clickthroughrate = new Task('Click-Through Rate', '[insert click-through rate description here]');
-  static image_object_detection = new Task('Object Detection', 'This model is trained to detect multiple objects in an image.');
-  static image_semantic_segmentation = new Task('Semantic Segmentation', '[insert semantic segmentation description here]');
-  static image_instance_segmentation = new Task('Instance Segmentation', '[insert instance segmentation description here]');
-  static image_enhancement = new Task('Image Enhancement', '[insert image enhancement description here]');
+  static image_object_detection = new Task('Object Detection', 'Used to recognize and locate multiple objects in an image (for example, humans, animals, cars) and can help you answer what is in the image and where in the image is it located');
+  static image_semantic_segmentation = new Task('Semantic Segmentation', 'Used to segment out different parts of an image that belong to the same class. Can help you answer what is in the image, where in the image is it located, and if similar object overlap');
+  static image_instance_segmentation = new Task('Instance Segmentation', 'Used to segment out every distinct object in an image. Can help you answer what is in the image, where in the image it is located, and how many distinct objects there are.');
+  static image_enhancement = new Task('Image Enhancement', 'Used to sharpen an image and bring out details. Can help you focus or pick out important features in an image.');
 
   constructor(taskName, taskDescription, modelPageDescription = null) {
     this.name = taskName;
