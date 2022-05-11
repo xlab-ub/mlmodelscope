64ec9fce02f2868503dbe5c11c96a050c80cad5f
andrew <andrew@pixotech.com>
2022-05-11 16:41:33 -0500

    Update subtitle for instance segmentation
    C3SR-336

--- src/components/Experiment/QuickOutput/Outputs/InstanceSegmentation/InstanceSegmentation.js
+++ src/components/Experiment/QuickOutput/Outputs/InstanceSegmentation/InstanceSegmentation.js
@@ -5,11 +5,14 @@ import InstanceSegmentationTable from "./InstanceSegmentationTable";
 import InstanceSegmentationImage from "./InstanceSegmentationImage";
 import NoPredictions from "../_Common/components/NoPredictions";
 import "./InstanceSegmentation.scss";
+import Task from "../../../../../helpers/Task";
 
 
 export default function InstanceSegmentation(props) {
   const {getElement, getBlock} = useBEMNaming("instance-segmentation");
   const {coloredSections, hover, category} = useInstanceSegmentationControl(props.trial);
+
+  const task = Task.image_instance_segmentation;
 
   const getBody = () => {
     if (props.trial.results.responses[0].features.length === 0) return <NoPredictions modelId={props.trial.model.id}/>
@@ -29,7 +32,7 @@ export default function InstanceSegmentation(props) {
   return <div className={getBlock()}>
     <div className={getElement("header")}>
       <h3 className={getElement("header-headline")}>Output</h3>
-      <p className={getElement("header-subheading")}>What each object detected is</p>
+      <p className={getElement("header-subheading")}>{task.outputText}</p>
     </div>
     {getBody()}
     <div className={getElement("bottom-row")}>
