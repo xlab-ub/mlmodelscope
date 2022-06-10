2bf9a048b8f0e2da208977ec570cb84f7cda0a18
andrew <andrew@pixotech.com>
2022-06-10 10:10:10 -0500

    Fix "You filtered too hard!~!!" text

--- src/components/Experiment/QuickOutput/Outputs/_Common/components/TableNoRows.js
+++ src/components/Experiment/QuickOutput/Outputs/_Common/components/TableNoRows.js
@@ -6,7 +6,7 @@ export default function TableNoRows(props) {
   const {getBlock, getElement} = useBEMNaming("table-no-rows");
 
   return <div className={getBlock()}>
-    <p className={getElement("main-heading")}>You filtered too hard!~!!!</p>
+    <p className={getElement("main-heading")}>All results are hidden.</p>
     <p className={getElement("heading")}>Try adjusting confidence threshold.</p>
   </div>
 }
