d7dcc9580d1d01369b2cdbfb763d15117846c14d
Matthew Fotzler <mattf@pixotech.com>
2023-04-10 13:10:15 -0500

    Refactor ModelTag into a functional component
    C3SR-487

--- src/components/Common/ModelTag.jsx
+++ src/components/Common/ModelTag.jsx
@@ -4,30 +4,27 @@ import Task from "../../helpers/Task";
 import Framework from "../../helpers/Framework";
 import {ReactComponent as Microchip} from "../../resources/icons/microchip-solid.svg";
 
-export default class ModelTag extends Component {
-  render() {
-    let className = "model-tag";
-    if (!!this.props.type && this.props.type != "") {
-      className = `${className} -${this.props.type}`;
-    }
-    let Icon = (() => <></>);
-    let classes = "";
-    if (this.props.type === "task") {
-      let task = Task.getStaticTask(this.props.content);
-      Icon = task.Icon;
-    }
-    if (this.props.type === "framework") {
-      let framework = Framework.getStaticFramework(this.props.content);
-      Icon = framework.Icon;
-    }
-    if (this.props.type === "machine") {
-      Icon = (props) => <Microchip {...props}></Microchip>
-      classes = " model-tag__icon-machine"
-    }
-
-
-    return (
-      <span className={className}><Icon className={"model-tag__icon" + classes}/> {this.props.content}</span>
-    );
+export default function ModelTag(props) {
+  let className = "model-tag";
+  if (!!props.type && props.type != "") {
+    className = `${className} -${props.type}`;
   }
+  let Icon = (() => <></>);
+  let classes = "";
+  if (props.type === "task") {
+    let task = Task.getStaticTask(props.content);
+    Icon = task.Icon;
+  }
+  if (props.type === "framework") {
+    let framework = Framework.getStaticFramework(props.content);
+    Icon = framework.Icon;
+  }
+  if (props.type === "machine") {
+    Icon = (props) => <Microchip {...props}></Microchip>
+    classes = " model-tag__icon-machine"
+  }
+
+  return (
+    <span className={className}><Icon className={"model-tag__icon" + classes}/> {props.content}</span>
+  );
 }
