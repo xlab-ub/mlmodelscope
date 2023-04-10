0905db959198a6282c5ff94253f6dcef8908f03a
Matthew Fotzler <mattf@pixotech.com>
2023-04-10 13:45:20 -0500

    Convert SortButton into functional component
    C3SR-487

--- src/components/Buttons/SortButton.jsx
+++ src/components/Buttons/SortButton.jsx
@@ -1,57 +1,38 @@
-import React, {Component} from "react";
+import React, {useState} from "react";
 import {ReactComponent as SortBars} from "../../resources/icons/sort-bars.svg";
 
-export default class SortButton extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      optionsHidden: true,
-    }
-  }
+export default function SortButton(props) {
+  const [optionsHidden, setOptionsHidden] = useState(true);
 
-  showOptions = () => {
-    this.setState({
-      optionsHidden: false
-    });
-  }
-
-  hideOptions = () => {
-    this.setState({
-      optionsHidden: true,
-    });
-  }
-
-  preventFocus = (e) => {
+  const preventFocus = (e) => {
     e.preventDefault();
   }
 
-  onOptionClick = (isAscending) => {
-    if (!!this.props.updateSortByNameIsAscending) {
-      this.props.updateSortByNameIsAscending(isAscending);
+  const onOptionClick = (isAscending) => {
+    if (!!props.updateSortByNameIsAscending) {
+      props.updateSortByNameIsAscending(isAscending);
     }
     document.activeElement.blur();
-    this.hideOptions();
+    setOptionsHidden(true);
   }
 
-  render() {
-    return (
-      <div className="sort-button">
-        <button className="sort-button__button" onClick={this.showOptions} onFocus={this.showOptions}
-                onBlur={this.hideOptions}>
-          <SortBars className="sort-button__icon"/>
-          Sort by
-        </button>
-        <div className="sort-button__option-box" hidden={this.state.optionsHidden}>
-          <input className="sort-button__radio" type="radio" id="name-ascend" name="sort-option" value="true"
-                 defaultChecked={this.props.isSortAscending}/>
-          <label className="sort-button__option" htmlFor="name-ascend" onMouseDown={this.preventFocus}
-                 onClick={() => this.onOptionClick(true)}>Name ascending</label>
-          <input className="sort-button__radio" type="radio" id="name-descend" name="sort-option" value="false"
-                 defaultChecked={!this.props.isSortAscending}/>
-          <label className="sort-button__option" htmlFor="name-descend" onMouseDown={this.preventFocus}
-                 onClick={() => this.onOptionClick(false)}>Name descending</label>
-        </div>
+  return (
+    <div className="sort-button">
+      <button className="sort-button__button" onClick={() => setOptionsHidden(false)} onFocus={() => setOptionsHidden(false)}
+              onBlur={() => setOptionsHidden(true)}>
+        <SortBars className="sort-button__icon"/>
+        Sort by
+      </button>
+      <div className="sort-button__option-box" hidden={optionsHidden}>
+        <input className="sort-button__radio" type="radio" id="name-ascend" name="sort-option" value="true"
+               defaultChecked={props.isSortAscending}/>
+        <label className="sort-button__option" htmlFor="name-ascend" onMouseDown={preventFocus}
+               onClick={() => onOptionClick(true)}>Name ascending</label>
+        <input className="sort-button__radio" type="radio" id="name-descend" name="sort-option" value="false"
+               defaultChecked={!props.isSortAscending}/>
+        <label className="sort-button__option" htmlFor="name-descend" onMouseDown={preventFocus}
+               onClick={() => onOptionClick(false)}>Name descending</label>
       </div>
-    );
-  }
+    </div>
+  );
 }
