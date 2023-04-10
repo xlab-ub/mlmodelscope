2c4a27a878b430c0010b8b157374bbfeb65ed22b
andrew <andrew@pixotech.com>
2023-04-10 15:37:04 -0500

    Refactor PageNavigationSummary into a function component
    C3SR-487

--- src/components/Paginator/PageNavigationSummary.js
+++ src/components/Paginator/PageNavigationSummary.js
@@ -1,38 +1,43 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './PageNavigationSummary.scss';
 import Icon from "../Icon/Icon";
 
-export default class PageNavigationSummary extends Component {
-  static defaultProps = {
-    data: [],
-    pageCount: 1,
-    selectedPage: 1,
-    totalCount: 0,
-    selectPage: () => {},
-  }
-  render() {
-    let searchSuffix = !this.props.searchText ? "" : ` for "${this.props.searchText}"`;
+const defaultProps = {
+  data: [],
+  pageCount: 1,
+  selectedPage: 1,
+  totalCount: 0,
+  selectPage: () => {
+  },
+};
 
-    return <div className="page-nav-summary">
-      <div className="page-nav-summary__text">
-        page <strong>{this.props.selectedPage} of {this.props.pageCount}</strong>, showing <strong>{this.props.data.length} of {this.props.totalCount} models</strong>{searchSuffix}
-      </div>
-      <div className="page-nav-summary__buttons">
-        <button className="page-nav-summary__prev-button" onClick={() => this.selectPreviousPage()} disabled={this.props.selectedPage === 1} aria-label="Previous page">
-          <Icon icon="arrow" />
-        </button>
-        <button className="page-nav-summary__next-button" onClick={() => this.selectNextPage()} disabled={this.props.selectedPage === this.props.pageCount} aria-label="Next page">
-          <Icon icon="arrow" />
-        </button>
-      </div>
+export default function PageNavigationSummary(givenProps) {
+  const props = {...defaultProps, ...givenProps};
+
+  const selectNextPage = () => {
+    props.selectPage(Math.min(props.pageCount, props.selectedPage + 1));
+  }
+
+  const selectPreviousPage = () => {
+    props.selectPage(Math.max(1, props.selectedPage - 1));
+  }
+
+  let searchSuffix = !props.searchText ? "" : ` for "${props.searchText}"`;
+
+  return <div className="page-nav-summary">
+    <div className="page-nav-summary__text">
+      page <strong>{props.selectedPage} of {props.pageCount}</strong>,
+      showing <strong>{props.data.length} of {props.totalCount} models</strong>{searchSuffix}
     </div>
-  }
-
-  selectNextPage() {
-    this.props.selectPage(Math.min(this.props.pageCount, this.props.selectedPage + 1));
-  }
-
-  selectPreviousPage() {
-    this.props.selectPage(Math.max(1, this.props.selectedPage - 1));
-  }
+    <div className="page-nav-summary__buttons">
+      <button className="page-nav-summary__prev-button" onClick={() => selectPreviousPage()}
+              disabled={props.selectedPage === 1} aria-label="Previous page">
+        <Icon icon="arrow"/>
+      </button>
+      <button className="page-nav-summary__next-button" onClick={() => selectNextPage()}
+              disabled={props.selectedPage === props.pageCount} aria-label="Next page">
+        <Icon icon="arrow"/>
+      </button>
+    </div>
+  </div>
 }
