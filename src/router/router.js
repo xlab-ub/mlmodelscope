627f2449054d6a129f3b2e92ea288b99fe934a06
andrew <andrew@pixotech.com>
2023-04-10 16:22:18 -0500

    Refactor Router into a functional component
    C3SR-487

--- src/router/router.js
+++ src/router/router.js
@@ -1,16 +1,14 @@
-import React, {Component} from "react";
+import React from "react";
 import Routes from "./config";
 import {Route, Switch} from "react-router-dom";
 
-export default class Router extends Component {
-  render() {
+export default function Router() {
     return (
-      <Switch>
-        {
-          Routes.map((route, i) =>
-            <Route exact={route.exact !== false} key={i} path={route.path} component={route.component}/>)
-        }
-      </Switch>
+        <Switch>
+            {
+                Routes.map((route, i) =>
+                    <Route exact={route.exact !== false} key={i} path={route.path} component={route.component}/>)
+            }
+        </Switch>
     );
-  }
 }
