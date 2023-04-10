3c03cdadff9a24f48e1cb1c7371d7ece08242c9c
Matthew Fotzler <mattf@pixotech.com>
2023-04-10 16:24:41 -0500

    Convert PredictionExpander to a functional component
    C3SR-487

--- src/components/Common/PredictionExpander.js
+++ src/components/Common/PredictionExpander.js
@@ -1,73 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Prediction from "../Experiment/QuickOutput/Outputs/Classification/Prediction";
-import BEMComponent from "./BEMComponent";
 import "./PredictionExpander.scss";
+import useBEMNaming from "../../common/useBEMNaming";
 
-export default class PredictionExpander extends BEMComponent {
-  static defaultProps = {
-    className: "prediction-expander",
-    predictions: []
-  }
+const defaultProps = {
+  className: "prediction-expander",
+  predictions: []
+};
 
-  constructor(props) {
-    super(props);
+export default function PredictionExpander(givenProps) {
+  const props = {...defaultProps, ...givenProps};
+  const predictions = props.predictions.filter(prediction => (prediction.probability * 100) > (props.globalValue ?? -1));
+  const { getBlock, getElement } = useBEMNaming(props.className);
+  const [predictionsExpanded, setPredictionsExpanded] = useState(!!props.showAll);
 
-    this.state = {
-      predictionsExpanded: this.props.showAll === true
-    }
-
-    this.modifiers = {
-      expand: {
-        collapsed: (state) => !state.predictionsExpanded
-      },
-      "prediction-overflow": {
-        collapsed: (state) => !state.predictionsExpanded
-      }
-    }
-  }
-
-  filterPredictions() {
-    let globalValue = this.props.globalValue ?? -1;
-    return this.props.predictions.filter(prediction => (prediction.probability * 100) > globalValue);
-  }
-
-  render() {
-    let predictions = this.filterPredictions();
-
-    return (
-      <div className={this.block()}>
-        <div className={this.element('predictions')}>
-          {predictions.slice(1, 3).map(this.makePrediction)}
-
-          <div hidden={!this.props.showAll} className={this.element('prediction-overflow')}>
-            {predictions.slice(3).map(this.makePrediction)}
-          </div>
-        </div>
-        {this.makeExpanderButton()}
-      </div>
-    )
-  }
-
-  makePrediction = (feature, index) => {
+  const makePrediction = (feature, index) => {
     return <Prediction key={index} feature={feature}/>
-  }
+  };
 
-  makeExpanderButton() {
-    if (!this.props.showAll && this.props.predictions.length > 3) {
+  const makeExpanderButton = () => {
+    if (!props.showAll && props.predictions.length > 3) {
       return (
-        <button className={this.element('expand')} onClick={this.expandClicked}>{this.makeExpanderLabel()}</button>)
+        <button className={getElement('expand')} onClick={expandClicked}>{makeExpanderLabel()}</button>)
     }
   }
 
-  makeExpanderLabel = () => {
-    return `${this.state.predictionsExpanded ? 'Hide' : 'Show'} all predictions`;
+  const makeExpanderLabel = () => {
+    return `${predictionsExpanded ? 'Hide' : 'Show'} all predictions`;
   }
 
-  expandClicked = () => {
-    if (this.props.toggleShowAll) this.props.toggleShowAll();
+  const expandClicked = () => {
+    if (props.toggleShowAll) props.toggleShowAll();
     else
-      this.setState({
-        predictionsExpanded: !this.state.predictionsExpanded
-      });
+      setPredictionsExpanded(!predictionsExpanded);
   }
+
+  return (
+    <div className={getBlock()}>
+      <div className={getElement('predictions')}>
+        {predictions.slice(1, 3).map(makePrediction)}
+
+        <div hidden={!props.showAll} className={getElement('prediction-overflow')}>
+          {predictions.slice(3).map(makePrediction)}
+        </div>
+      </div>
+      {makeExpanderButton()}
+    </div>
+  );
 }
