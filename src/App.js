6363d605eb3d43d614c16c624990ef3f6b25895e
andrew & matt <andrew@pixotech.com>
2023-04-10 10:59:44 -0500

    Refactor the App component into a functional component
    C3SR-487

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import "./Stopgap.scss";
-import React, { Component } from "react";
+import React from "react";
 import Footer from "./components/Footer/Footer";
 import Router from "./router/router";
-import { BrowserRouter as ReactRouter } from "react-router-dom";
+import {BrowserRouter as ReactRouter} from "react-router-dom";
 
-class App extends Component {
-
-  getSiteTitle= () => { return process.env.REACT_APP_SITE_TITLE || "MLModelscope"; }
-
-  render() {
-
+export default function App() {
     console.log("App GIT Commit: " + process.env.REACT_APP_VERSION);
 
     return (
-      <div>
-        <ReactRouter>
-          <Router />
-          <Footer />
-        </ReactRouter>
-      </div>
+        <div>
+            <ReactRouter>
+                <Router/>
+                <Footer/>
+            </ReactRouter>
+        </div>
     );
-  }
 }
-
-export default App;
