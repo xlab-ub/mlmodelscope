2a9f04600bf1a115fb4112eabfc3e331d710a41b
andrew & matt <andrew@pixotech.com>
2023-04-10 11:42:13 -0500

    Rename an icon component to a more descriptive name

--- src/components/ModelList/ModelList.ResponsiveFilterContent.js
+++ src/components/ModelList/ModelList.ResponsiveFilterContent.js
@@ -1,33 +1,34 @@
 import {ReactComponent as SortBars} from "../../resources/icons/sort-bars.svg";
-import {ReactComponent as X} from "../../resources/icons/x.svg";
+import {ReactComponent as XIcon} from "../../resources/icons/x.svg";
 import FilterPanel from "./FilterPanel";
 import React, {useState} from "react";
 
 export function ModelListResponsiveFilterContent(props: { showFilterMenu: boolean | any, closeFilter: () => void, filterGroups: any, toggleFilter: any }) {
-  const [isClosing, setIsClosing] = useState(false);
+    const [isClosing, setIsClosing] = useState(false);
 
-  const onClose = () => {
-    setIsClosing(true);
+    const onClose = () => {
+        setIsClosing(true);
 
-    setTimeout(() => {
-      props.closeFilter();
-      setIsClosing(false);
-    }, 250);
-  }
+        setTimeout(() => {
+            props.closeFilter();
+            setIsClosing(false);
+        }, 250);
+    }
 
-  return <div hidden={!props.showFilterMenu} aria-hidden={!props.showFilterMenu}
-              className={`model-list-page__filter-content ${isClosing && "model-list-page__filter-content-closing"}`}>
-    <div className={"model-list-page__filter-content-header"}>
-      <p className={"model-list-page__filter-content-header-title"}>
-        <SortBars/>{" "}Filter models by
-      </p>
-      <button className={"model-list-page__filter-content-header-close"} onClick={onClose}>
-        <X/>
-      </button>
-    </div>
-    <div className={"model-list-page__filter-content-body"}>
-      <FilterPanel hideTasks={props.hideTasks} className="model-list-page__filters" filterGroups={props.filterGroups}
-                   toggleFilter={props.toggleFilter} hidePanelHeader/>
-    </div>
-  </div>;
+    return <div hidden={!props.showFilterMenu} aria-hidden={!props.showFilterMenu}
+                className={`model-list-page__filter-content ${isClosing && "model-list-page__filter-content-closing"}`}>
+        <div className={"model-list-page__filter-content-header"}>
+            <p className={"model-list-page__filter-content-header-title"}>
+                <SortBars/>{" "}Filter models by
+            </p>
+            <button className={"model-list-page__filter-content-header-close"} onClick={onClose}>
+                <XIcon/>
+            </button>
+        </div>
+        <div className={"model-list-page__filter-content-body"}>
+            <FilterPanel hideTasks={props.hideTasks} className="model-list-page__filters"
+                         filterGroups={props.filterGroups}
+                         toggleFilter={props.toggleFilter} hidePanelHeader/>
+        </div>
+    </div>;
 }
