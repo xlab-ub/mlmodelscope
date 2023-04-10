8fbd81abfb70aa62cecccc8b4acd2b3c8dc79de8
andrew <andrew@pixotech.com>
2023-04-10 15:30:15 -0500

    Refactor ModelHeader into a function component
    C3SR-487

---
+++ src/components/ModelList/ModelHeader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function ModelHeader() {
+    return (
+        <div className="model-header">
+            <h1 className="model-header__title"> Browse models </h1>
+            <p className="model-header__description"> Find models to compare. </p>
+        </div>
+    );
+}
+
+
