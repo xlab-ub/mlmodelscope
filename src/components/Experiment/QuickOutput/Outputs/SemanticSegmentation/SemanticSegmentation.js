af85cb6988388d50b3a5368380d1c1e88b9fe55d
andrew & matt <andrew@pixotech.com>
2023-04-17 10:40:01 -0500

    fix issue with SemanticSegmentation erroring when it should display the "no predictions found" UI
    C3SR-487

--- src/components/Experiment/QuickOutput/Outputs/SemanticSegmentation/SemanticSegmentation.js
+++ src/components/Experiment/QuickOutput/Outputs/SemanticSegmentation/SemanticSegmentation.js
@@ -9,46 +9,48 @@ import NoPredictions from "../_Common/components/NoPredictions";
 import OutputDuration from "../_Common/components/OutputDuration";
 
 export default function SemanticSegmentation(props) {
-  const {
-    hover,
-    labelToShow,
-    usedLabels,
-    height,
-    image,
-    int_mask,
-    width,
-    categories
-  } = useSemanticSegmentationControl(props.trial);
-  const {getElement, getBlock} = useBEMNaming("semantic-segmentation");
-  const task = Task.image_semantic_segmentation;
+    const {
+        hover,
+        labelToShow,
+        usedLabels,
+        height,
+        image,
+        int_mask,
+        width,
+        categories
+    } = useSemanticSegmentationControl(props.trial);
+    const {getElement, getBlock} = useBEMNaming("semantic-segmentation");
+    const task = Task.image_semantic_segmentation;
 
-  if (usedLabels === null || usedLabels === undefined || usedLabels.length === 0) {
-    return <div className={getBlock()}><NoPredictions/></div>
-  }
+    const feature = props.trial.results.responses[0].features[0];
+    if (!feature || usedLabels === null || usedLabels === undefined || usedLabels.length === 0) {
+        return <div className={getBlock()}><NoPredictions/></div>
+    }
 
-  return <div className={getBlock()}>
-    <div className={getElement("header")}>
-      <div className={getElement("header-row")}>
-        <h3 className={getElement("header-headline")}>Output</h3>
-        <OutputDuration duration={props.trial.results.duration}/>
-      </div>
-      <p className={getElement("header-subheading")}>{task.outputText}</p>
+    return <div className={getBlock()}>
+        <div className={getElement("header")}>
+            <div className={getElement("header-row")}>
+                <h3 className={getElement("header-headline")}>Output</h3>
+                <OutputDuration duration={props.trial.results.duration}/>
+            </div>
+            <p className={getElement("header-subheading")}>{task.outputText}</p>
+        </div>
+        <div className={getElement("top-row")}>
+            <SemanticSegmentationImage img={image}
+                                       width={width}
+                                       height={height}
+                                       int_mask={int_mask}
+                                       hover={hover}
+                                       categories={categories}
+                                       labelToShow={labelToShow}
+            />
+            <SemanticSegmentationTable labels={usedLabels} hover={hover} labelToShow={labelToShow}
+                                       categories={categories}/>
+        </div>
+        <div className={getElement("bottom-row")}>
+            <button onClick={props.onBackClicked} className={getElement("bottom-row-btn")}>
+                Try a different image
+            </button>
+        </div>
     </div>
-    <div className={getElement("top-row")}>
-      <SemanticSegmentationImage img={image}
-                                 width={width}
-                                 height={height}
-                                 int_mask={int_mask}
-                                 hover={hover}
-                                 categories={categories}
-                                 labelToShow={labelToShow}
-      />
-      <SemanticSegmentationTable labels={usedLabels} hover={hover} labelToShow={labelToShow} categories={categories}/>
-    </div>
-    <div className={getElement("bottom-row")}>
-      <button onClick={props.onBackClicked} className={getElement("bottom-row-btn")}>
-        Try a different image
-      </button>
-    </div>
-  </div>
 }
--- src/components/Experiment/QuickOutput/Outputs/SemanticSegmentation/hooks/useSemanticSegmentationControl.js
+++ src/components/Experiment/QuickOutput/Outputs/SemanticSegmentation/hooks/useSemanticSegmentationControl.js
@@ -2,23 +2,29 @@ import {useHoverControl} from "../../_Common/hooks/useHoverControl";
 import {useCategoryFilters} from "../../_Common/hooks/useCategoryFilters";
 
 export default function useSemanticSegmentationControl(trial) {
-  const hover = useHoverControl();
-  let labelToShow = null;
+    const hover = useHoverControl();
+    let labelToShow = null;
 
-  if (hover.property) {
-    labelToShow = trial.results.responses[0].features[0].semantic_segment.labels[hover.property];
-  }
+    const feature = trial.results.responses[0].features[0];
+    if (hover.property) {
+        labelToShow = feature?.semantic_segment.labels[hover.property] ?? "";
+    }
 
-  const image = trial.inputs[0];
-  const {height, int_mask, labels, width} = trial.results.responses[0].features[0].semantic_segment;
-  const usedLabels = (labels || []).map((label, index) => {
-    return index > 0 && int_mask.indexOf(index) > -1
-      ? {index: index - 1, label}
-      : null;
-  }).filter(l => l !== null);
+    const image = trial.inputs[0];
+    const {height, int_mask, labels, width} = feature?.semantic_segment ?? {
+        height: 0,
+        int_mask: [],
+        labels: [],
+        width: 0
+    };
+    const usedLabels = (labels || []).map((label, index) => {
+        return index > 0 && int_mask.indexOf(index) > -1
+            ? {index: index - 1, label}
+            : null;
+    }).filter(l => l !== null);
 
-  const categories = useCategoryFilters(usedLabels, "index")
+    const categories = useCategoryFilters(usedLabels, "index")
 
 
-  return {hover, labelToShow, image, height, usedLabels, width, int_mask, categories};
+    return {hover, labelToShow, image, height, usedLabels, width, int_mask, categories};
 }
