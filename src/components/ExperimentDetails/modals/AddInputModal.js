03b2d362b9bbe8278a106244814198f366c6bbc4
andrew <andrew@pixotech.com>
2023-05-03 11:13:03 -0500

    re-add sample inputs to model comparison inputs

--- src/components/ExperimentDetails/modals/AddInputModal.js
+++ src/components/ExperimentDetails/modals/AddInputModal.js
@@ -6,18 +6,18 @@ import {image_classification} from "../../../helpers/TaskIDs";
 import "./AddInputModal.scss";
 
 export default function AddInputModal(props) {
-  const {getElement} = useBEMNaming("add-input-modal");
+    const {getElement} = useBEMNaming("add-input-modal");
 
-  const onAdd = (urls) => {
-    props.addInput(urls);
-    props.close();
-  }
+    const onAdd = (urls) => {
+        props.addInput(urls);
+        props.close();
+    }
 
-  return <Modal onCancel={props.close}>
-    <p className={getElement("heading")}> Choose an input </p>
-    <div className={getElement("input-wrapper")}>
-      <QuickInput model={{output: {type: image_classification}}} hideHeader
-                  onRunModelClicked={onAdd} sampleInputs={props.sampleInputs} multiple hideSample/>
-    </div>
-  </Modal>
+    return <Modal onCancel={props.close}>
+        <p className={getElement("heading")}> Choose an input </p>
+        <div className={getElement("input-wrapper")}>
+            <QuickInput model={{output: {type: image_classification}}} hideHeader
+                        onRunModelClicked={onAdd} sampleInputs={props.sampleInputs} multiple/>
+        </div>
+    </Modal>
 }
