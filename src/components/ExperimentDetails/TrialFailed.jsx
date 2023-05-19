4c6d52d731c11469dac0981e4bb14d2dafabeb5b
andrew & tyler <andrew@pixotech.com>
2023-05-19 09:38:53 -0500

    Minor style tweaks to process/trial failed components

--- src/components/ExperimentDetails/TrialFailed.jsx
+++ src/components/ExperimentDetails/TrialFailed.jsx
@@ -11,7 +11,6 @@ export default function TrialFailed() {
                 <ErrorIcon/>
                 <p className={getElement("box-heading")}>Process Failed</p>
             </div>
-
         </div>
     )
 }
