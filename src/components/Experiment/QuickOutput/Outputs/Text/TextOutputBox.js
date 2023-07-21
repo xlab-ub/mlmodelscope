fbf7e282c2eece052c46e272b74d224b012e2588
andrew <andrew@pixotech.com>
2023-07-21 11:21:31 -0500

    Add help text to text output box

--- src/components/Experiment/QuickOutput/Outputs/Text/TextOutputBox.js
+++ src/components/Experiment/QuickOutput/Outputs/Text/TextOutputBox.js
@@ -2,9 +2,12 @@ import useBEMNaming from "../../../../../common/useBEMNaming";
 import OutputDuration from "../_Common/components/OutputDuration";
 import Rating from "../Classification/Rating";
 import React from "react";
+import Task from "../../../../../helpers/Task";
+import { text } from "../../../../../helpers/TaskIDs";
 
 export function TextOutputBox(props) {
   const { getElement } = useBEMNaming("text-output");
+  const task = Task.getStaticTask(text);
 
   return (
     <div className={getElement("results")}>
@@ -12,7 +15,7 @@ export function TextOutputBox(props) {
         <h3 className={getElement("title-row-title")}>Output</h3>
         <OutputDuration duration={props.duration} />
       </div>
-
+      <p className={getElement("subtitle")}>{task.outputText}</p>
       <div className={getElement("output-container")}>
         <p className={getElement("output-container-text")}>{props.output}</p>
       </div>
