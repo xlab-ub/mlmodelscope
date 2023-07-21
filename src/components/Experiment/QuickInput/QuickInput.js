6b199c2dbcff1e784667471167e9c32615a6bfd3
andrew <andrew@pixotech.com>
2023-07-21 11:00:51 -0500

    refactor QuickInput to use the task's input type instead of the task id

--- src/components/Experiment/QuickInput/QuickInput.js
+++ src/components/Experiment/QuickInput/QuickInput.js
@@ -1,26 +1,18 @@
 import React from "react";
 import "./QuickImageInput.scss";
-import {
-  image_classification,
-  image_enhancement,
-  instance_segmentation,
-  object_detection,
-  semantic_segmentation,
-  text,
-} from "../../../helpers/TaskIDs";
 import QuickImageInput from "./QuickImageInput";
 import QuickTextInput from "./QuickTextInput";
+import Task from "../../../helpers/Task";
+import { TaskInputTypes } from "../../../helpers/TaskInputTypes";
 
 export default function QuickInput(props) {
-  switch (props.model.output.type) {
-    case text:
+  const inputType = Task.getStaticTask(props.model.output.type).inputType;
+
+  switch (inputType) {
+    case TaskInputTypes.Text:
       return <QuickTextInput {...props} />;
 
-    case image_classification:
-    case object_detection:
-    case image_enhancement:
-    case semantic_segmentation:
-    case instance_segmentation:
+    case TaskInputTypes.Image:
     default:
       return <QuickImageInput {...props} />;
   }
