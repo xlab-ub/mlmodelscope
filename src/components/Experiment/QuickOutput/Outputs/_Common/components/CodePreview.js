b00e8c215537a238c445b9fd1975e71bd6a2f1a4
andrew <andrew@pixotech.com>
2023-10-04 09:51:41 -0500

    Minor refactorings to make theme switching a little easier

--- src/components/Experiment/QuickOutput/Outputs/_Common/components/CodePreview.js
+++ src/components/Experiment/QuickOutput/Outputs/_Common/components/CodePreview.js
@@ -1,14 +1,10 @@
 import SyntaxHighlighter from "react-syntax-highlighter";
 import React from "react";
-import { atelierForestDark } from "react-syntax-highlighter/dist/cjs/styles/hljs";
+import { atelierForestDark as theme } from "react-syntax-highlighter/dist/cjs/styles/hljs";
 
 export function CodePreview(props) {
   return (
-    <SyntaxHighlighter
-      style={atelierForestDark}
-      showLineNumbers
-      language="python"
-    >
+    <SyntaxHighlighter style={theme} showLineNumbers language="python">
       {props.children}
     </SyntaxHighlighter>
   );
